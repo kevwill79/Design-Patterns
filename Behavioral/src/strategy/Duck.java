7a36ba6e7fb276800c1f91ec13509bf1b59f5653
kevwill79 <dmx36502@yahoo.com>
2020-03-29 00:30:18 -0700

    Finished implementation

--- Behavioral/src/strategy/Duck.java
+++ Behavioral/src/strategy/Duck.java
@@ -1,5 +1,30 @@
 package strategy;
 
-public class Duck {
+import strategy.encapsulatedSoundBehavior.Quack;
+import strategy.encapsulatedSpecies.WaterFowl;
+import strategy.encapsulatedWalkingBehavior.WebbedFeet;
 
+public class Duck extends Animal{
+	
+	public Duck() {
+		/* When a Duck object is instantiated, this constructor initializes
+		/* the inherited behavior variables to duck specific behaviors
+		 */
+		species = new WaterFowl();
+		soundBehavior = new Quack();
+		walkingBehavior = new WebbedFeet();
+		
+		//Behaviors that are the same for all animals
+		eat();
+		sleep();
+	}
+
+	@Override
+	public void displayMyBehaviors() {
+		speciesType();
+		makeSound();
+		walkingStyle();
+		System.out.println();
+		
+	}
 }
